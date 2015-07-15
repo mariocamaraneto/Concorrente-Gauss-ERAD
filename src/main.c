80a07be70d8eabed440ec1ea8e9ad83378a5a5e4
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-07-14 22:15:36 -0300

    codigo funcional

--- src/main.c
+++ src/main.c
@@ -6,37 +6,44 @@
 #include <string.h>
 #include <sched.h>
 
+#define max_size 10001
 
-void TrocarLinhas(int n, int latual, int ltrocar, double **A, double *b)
+double A[max_size][max_size];
+double B[max_size];
+double x[max_size];
+int tamanho;
+
+
+void TrocarLinhas( int latual, int ltrocar)
 {
     double aux;
     int i;
-    for(i = latual; i <= n; i++) //Substitui as linhas
+    for(i = latual; i <= tamanho; ++i) //Substitui as linhas
     {
         aux = A[latual][i]; //Guarda valor da linha original
         A[latual][i] = A[ltrocar][i]; //Troca pelo valor da outra linha
         A[ltrocar][i] = aux; //Restaura o valor da linha original posicionado em outra linha
     }
-    aux = b[latual]; //Mesmo processo com o vetor b
-    b[latual] = b[ltrocar];
-    b[ltrocar] = aux;
+    aux = B[latual]; //Mesmo processo com o vetor b
+    B[latual] = B[ltrocar];
+    B[ltrocar] = aux;
 
 }
 
-void SubsRetroativa(int n, double **A, double *b, double *x)
+void SubsRetroativa()
 {
 
-    double soma[n];
+    double soma[tamanho];
     int i,j;
-    for(i = n; i > 0; i--)
+    for(i = tamanho; i > 0; i--)
     {
 
     	soma[i] = 0;
-		#pragma omp parallel shared(A,b,n, soma) private(j)
+		#pragma omp parallel shared(A,B,tamanho, soma) private(j)
         {
 
 			#pragma omp for schedule(guided,1)
-			for(j = 1 + i; j <= n; j++){
+			for(j = 1 + i; j <= tamanho; ++j){
 				double aux = A[i][j] * x[j];
 				#pragma omp atomic
 					soma[i]+=aux;
@@ -44,71 +51,69 @@ void SubsRetroativa(int n, double **A, double *b, double *x)
 			}
         }
 
-        x[i] = (b[i] - soma[i]) / A[i][i];
-
+        x[i] = (B[i] - soma[i]) / A[i][i];
     }
 
 }
 
-void Imprimir_Vetor(int n, double *b)
+void Imprimir_Vetor()
 {
     int i;
 
-    for(i = 1; i <= n; i++){
-        printf("%2.4f\n", b[i]);
+    for(i = 1; i <= tamanho; ++i){
+        printf("%2.4f\n", x[i]);
     }
     printf("\n");
 }
 
-void MEG(int n, double **A, double *b, double *x, int pivotamento) //Pivotamento = 0 -> MEG sem pivotamento; Pivotamento = 1 -> MEG com pivotamento
+void MEG() //Pivotamento = 0 -> MEG sem pivotamento; Pivotamento = 1 -> MEG com pivotamento
 {
     int i, j, k;
     double pivo, m;
 
 
-    for(k = 1; k < n; k++) // k = Coluna a ser zerada
+    for(k = 1; k < tamanho; ++k) // k = Coluna a ser zerada
     {
         pivo = A[k][k];
 
-        if(!pivo) //Se o elemento pivô for 0, fará a troca da linha atual pela próxima e voltará a iteração
+        if(pivo) //Se o elemento pivô for 0, fará a troca da linha atual pela próxima e voltará a iteração
         {
-            for(i = 1 + k; i <= n; i++){ //Acha um pivô diferente de 0
-                if(A[i][k])
-                {
-                    pivo = A[i][k];
-                    TrocarLinhas(n, k, i, A, b);
-                    k--;
-                    break;
-                }
-            } //fecha for
+				for(i = 1 + k; i <= tamanho; i++) // i = Linha da Matriz; percorrer as linhas a fim de mudar valores de seus elementos
+				{
+					j = k;
+					m = A[i][j]/pivo; //Cálculo do multiplicador
+					A[i][j] = 0; //Zerando o elemento da coluna, a fim de evitar que as aproximações deixem o elemento com valor diferente de 0
+					for(j = 1 + k; j <= tamanho; j++){ // j = Coluna da Matriz; percorrer as colunas para calcular seus valores
+						double aux;
+						aux = ((m*A[k][j]) * -1);
+						//#pragma omp atomic
+						A[i][j] += aux; //Cálculo de todos os elementos da Linha
+					}
 
-            if(!pivo)//Caso todos os elementos abaixo o pivô sejam 0
-            {
-                printf("Nao foi possivel evitar divisao por 0 durante a utilizacao do metodo!\n");
-                exit(0);
-            }
-
+					B[i] = B[i] - m * B[k]; //Cálculo dos elementos de b;
+				 }
         }
-        else //Caso contrário, segue normalmente com a triangularização
+        else
         {
-            for(i = 1 + k; i <= n; i++) // i = Linha da Matriz; percorrer as linhas a fim de mudar valores de seus elementos
-            {
-                j = 0 + k;
-                m = A[i][j]/pivo; //Cálculo do multiplicador
-                A[i][j] = 0; //Zerando o elemento da coluna, a fim de evitar que as aproximações deixem o elemento com valor diferente de 0
-                for(j = 1 + k; j <= n; j++){ // j = Coluna da Matriz; percorrer as colunas para calcular seus valores
-                	double aux;
-                	aux = ((m*A[k][j]) * -1);
-					//#pragma omp atomic
-                    A[i][j] += aux; //Cálculo de todos os elementos da Linha
-                }
+        	 for(i = 1 + k; i <= tamanho; ++i){ //Acha um pivô diferente de 0
+				if(A[i][k])
+				{
+					pivo = A[i][k];
+					TrocarLinhas( k, i);
+					k--;
+					break;
+				}
+			} //fecha for
 
-                b[i] = b[i] - m * b[k]; //Cálculo dos elementos de b;
-             }
+			if(!pivo)//Caso todos os elementos abaixo o pivô sejam 0
+			{
+				printf("Nao foi possivel evitar divisao por 0 durante a utilizacao do metodo!\n");
+				exit(0);
+			}
         }
     }
 
-    for(i = 1; i <= n; i++){
+    for(i = 1; i <= tamanho; i++){
         if(!A[i][i])
         {
             printf("Matriz singular!\nNao é possível utilizar o método!\n");
@@ -117,108 +122,38 @@ void MEG(int n, double **A, double *b, double *x, int pivotamento) //Pivotamento
     }
 
     //Substituição retroativa
-    SubsRetroativa(n, A, b, x);
+    SubsRetroativa();
 
     //printf("Resolvendo o sistema Ax = b, obtemos o Vetor x:\n");
-    Imprimir_Vetor(n, x);
+    Imprimir_Vetor();
 }
 
 
 int main()
 {
-	int control;
-	int a;
-    int n;
-    int i;
-    int j;
-    char *valor;
-    int num;
-    char info[60000];
-    double **A;
-    double *B;
-    double *x;
-    FILE *arq;
+    int i, j;
 
-    arq = fopen("arquivo.txt","r");
-    if(arq == NULL){
-        		puts("Não foi possivel abrir o arquivo");
-    }
-    else {
-    num=0;
-    i=1;
-    control=0;
-    while((fgets(info, sizeof(info), arq) != NULL)){
+    printf("LENDO ENTRADA\n");
+    fflush(stdout);
+
+    scanf("%d", &tamanho);
 
 
-    	if(control==0){
-
-    		n = atoi(info);
-    		control++;
-    		A = (double **) malloc (n+1, sizeof(double *));
-    		for ( i = 0; i <= n; i++ ) {
-    		      A[i] = (double*) malloc (n+1, sizeof(double));
-
-    		      }
-    		B = (double *) malloc (n+1, sizeof(double *));
-    		x = (double *) malloc (n+1, sizeof(double *));
-    		valor = (char *) malloc (n+1, sizeof(double *));
-    		i=1;
-    	}
-    	else {
-    	a=0;
-    	if(control <= n){
-
-    		j=1;
-    		while(info[a]!= '\0'){
-
-    			num=0;
-    			while(info[a]!= ' ' && info[a]!='\0'){
-
-    				valor[num]=info[a];
-    				num++;
-    				a++;
-    			}
-    			;
-    			a++;
-    			valor[num]= '\0';
-    			num=0;
-    			A[i][j]= (double) atof(valor);
-    			//printf("%f",A[i][j]);
-    			j++;
-    		}
-    		i++;
-    		control++;
-
-    	}
-
-    	else{
-
-    		a=0;
-    		i=1;
-    		num=0;
-    		while(info[a]!= '\0'){
-
-    		    	num=0;
-    		    	while(info[a]!= ' ' && info[a]!='\0'){
-    		    			valor[num]=info[a];
-    		    			num++;
-    		    			a++;
-    		    	}
-    		    	a++;
-    		    	valor[num]= '\0';
-    		    	num=0;
-    		    	B[i]= (double)atof(valor);
-    		    	i++;
-    		    	}
-    		    	control++;
-    	}
-
+    for(i=1; i<=tamanho; ++i){
+    	for(j=1; j<=tamanho; ++j){
+    		scanf("%lf",&A[i][j]);
     	}
     }
-    }
-    fclose(arq);
 
-    MEG(n,A,B,x,0);
+    printf("LENDO matriz B\n");
+
+    for(i=1; i<=tamanho; ++i){
+    	scanf("%lf",&B[i]);
+    }
+
+    printf("PROCESSANDO\n");
+
+    MEG(tamanho);
 
     return 0;
 }
